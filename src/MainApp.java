b12a54e72f8e4793c2e7020350be549d83319fd2
ayushchaudhary55 <ayushchaudhary0494@gmail.com>
2026-04-23 01:46:22 +0530

    EV Charging System with Login UI (Developed by Ayush)

---
+++ src/MainApp.java
@@ -0,0 +1,11 @@
+// Smart EV Charging Station Mananger
+// Developed by Ayush
+
+import Page2.Main;
+
+public class MainApp {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
+//we have initiated page2 main from this home main
---
+++ src/Page9/Page9ViewController.java
@@ -0,0 +1,14 @@
+package Page9;
+
+public class Page9ViewController {
+
+}
+//CONNECTORS--**
+/*javafx.scene.Parent root=FXMLLoader.load(getClass().getResource("/Home/HomeView.fxml")); 
+	//OR
+	//Parent root=FXMLLoader.load(getClass().getClassLoader().getResource("marks/card/MarksCard.fxml")); 
+	javafx.scene.Scene scene = new javafx.scene.Scene(root);
+	javafx.stage.Stage stage=new javafx.stage.Stage();
+	stage.setScene(scene);
+	stage.show();
+}*/
